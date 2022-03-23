ecc1622dd5646b18cfd68de5561731d2253c1bff
Alejandro Vaquera Lopez <alejandrovaqueralopez608@gmail.com>
2022-03-23 04:13:58 -0700

    building endpoints of API

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const path = require("path");
 require("dotenv").config();//configuring dotenv package
-const API = process.env.API;
+
+const API_URL_EVENTS = process.env.API_URL_EVENTS;
+const API_KEY_LONGLAT= process.env.API_KEY_LONGLAT;
+const API_URL_LONGLAT= process.env.API_URL_LONGLAT;
+
+const API_URL_GEOMAP= process.env.API_URL_GEOMAP;
+const API_KEY_GEOMAP= process.env.API_KEY_GEOMAP;
+
 const app = express();
 const axios = require("axios");
 const fetchFile = require("./views/utils/fetchData.js");
@@ -14,17 +21,77 @@ app.get("/",(req,res)=>{
 	res.render(path.join(__dirname,"views/index.ejs"));
 })
 
-//fetchData
-app.get("/mars", async (req,res) =>{
+//using fetchData
+app.get("/events", async (req,res) =>{
 	try{
-		//const response = await fetchFile.fetchData(API);
-		//console.log(response.data);
-		fetchFile.fetchData(API);
-		var name = "holaaa";
+		//nasa api request
+		//objects with open status
+		let data1 = await fetchFile.fetchData(`${API_URL_EVENTS}?status=open`);
 
-		res.render(path.join(__dirname,"views/mars.ejs"),{name:name});
+		//event object entry
+		let eventData1 = data1.data.events[0];
+		let eventData2 = data1.data.events[1];
+		let eventData3 = data1.data.events[2];
+		let eventData4 = data1.data.events[3];
+		let eventData5 = data1.data.events[4];
+
+		//longlat api request
+		let longlatDetails1 = await fetchFile.longlatDetails(API_URL_LONGLAT,API_KEY_LONGLAT,eventData1.geometries[0].coordinates[1],eventData1.geometries[0].coordinates[0]);
+		let longlatDetails2 = await fetchFile.longlatDetails(API_URL_LONGLAT,API_KEY_LONGLAT,eventData2.geometries[0].coordinates[1],eventData2.geometries[0].coordinates[0]);
+		let longlatDetails3 = await fetchFile.longlatDetails(API_URL_LONGLAT,API_KEY_LONGLAT,eventData3.geometries[0].coordinates[1],eventData3.geometries[0].coordinates[0]);
+		let longlatDetails4 = await fetchFile.longlatDetails(API_URL_LONGLAT,API_KEY_LONGLAT,eventData4.geometries[0].coordinates[1],eventData4.geometries[0].coordinates[0]);
+		let longlatDetails5 = await fetchFile.longlatDetails(API_URL_LONGLAT,API_KEY_LONGLAT,eventData5.geometries[0].coordinates[1],eventData5.geometries[0].coordinates[0]);
+
+		//geoapify api request
+		let color1 = "9a08ff";
+		let color2 = "08ffdc";
+		let color3 = "9a08ff";
+		let color4 = "9a08ff";
+		let color5 = "9a08ff";
+
+	let geoMapMerge = await fetchFile.geoMapMergeFetch(
+		API_URL_GEOMAP,
+		API_KEY_GEOMAP,
+		"4000",
+		"4000",
+		"158",
+		"13",
+		eventData1.geometries[0].coordinates[0],
+		eventData1.geometries[0].coordinates[1],
+		color1,
+		eventData2.geometries[0].coordinates[0],
+		eventData2.geometries[0].coordinates[1],
+		color2,
+		eventData3.geometries[0].coordinates[0],
+		eventData3.geometries[0].coordinates[1],
+		color3,
+		eventData4.geometries[0].coordinates[0],
+		eventData4.geometries[0].coordinates[1],
+		color4,
+		eventData5.geometries[0].coordinates[0],
+		eventData5.geometries[0].coordinates[1],
+		color5,
+	);
+
+
+		//sending the objects to front via ejs
+		res.render(path.join(__dirname,"views/events.ejs"),{
+			eventSent1:eventData1,
+			eventSent2:eventData2,
+			eventSent3:eventData3,
+			eventSent4:eventData4,
+			eventSent5:eventData5,
+
+			longlatSent1:longlatDetails1.data.data[0],
+			longlatSent2:longlatDetails2.data.data[0],
+			longlatSent3:longlatDetails3.data.data[0],
+			longlatSent4:longlatDetails4.data.data[0],
+			longlatSent5:longlatDetails5.data.data[0],
+
+			geoMapMergeSent:geoMapMerge
+		});
 	}catch(error){
-		console.log(error);
+		console.log(new Error(`Error from API: ${error}`));
 	}
 })
 
--- src/views/utils/fetchData.js
+++ src/views/utils/fetchData.js
@@ -1,5 +1,6 @@
 const axios = require("axios");
 
+/*Generic fetch function*/
 let fetchData = async (API) => {
 	try{
 		const response = await axios.get(API)
@@ -8,4 +9,31 @@ let fetchData = async (API) => {
 		console.log(error)
 	}
 }
-module.exports = {fetchData};
+
+/*Longlat api fetch function*/
+let longlatDetails = async (API_URL_LONGLAT,API_KEY_LONGLAT,long,lat) => {
+	try{
+		const response = await axios.get(`${API_URL_LONGLAT}?access_key=${API_KEY_LONGLAT}&query=${long},${lat}`);
+		return response;
+	}catch(error){
+		console.log(error)
+	}
+}
+
+/*geomap code generator for marks*/
+let genMarkCode = (long,lat,color)=>{
+	let code = `marker=lonlat:${long},${lat};type:material;color:%23${color};icontype:awesome`;
+	return code;
+}
+
+/*geomap url creator function*/
+let geoMapMergeFetch = (API_URL_GEOMAP,API_KEY_GEOMAP,width,height,long,lat,latEvent1,longEvent1,color1,latEvent2,longEvent2,color2,latEvent3,longEvent3,color3,latEvent4,longEvent4,color4,latEvent5,longEvent5,color5) => {
+	try{
+		const response = (`${API_URL_GEOMAP}?style=osm-carto&width=${width}&height=${height}&center=lonlat:${long},${lat}&zoom=0&${genMarkCode(latEvent1,longEvent1,color1)}&${genMarkCode(latEvent2,longEvent2,color2)}&${genMarkCode(latEvent3,longEvent3,color3)}&${genMarkCode(latEvent4,longEvent4,color4)}&${genMarkCode(latEvent5,longEvent5,color5)}&apiKey=${API_KEY_GEOMAP}`);
+		return response;
+	}catch(error){
+		console.log(error)
+	}
+}
+
+module.exports = {fetchData,longlatDetails,geoMapMergeFetch};
